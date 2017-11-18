7816c2940dc88e82ba0de0253cd01a9936c05922
NeunZehn <nzdream@163.com>
2017-11-18 22:30:53 +0800

    Basic of test

---
+++ Assets/Scripts/AlignToActor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlignToActor : MonoBehaviour {
+
+	public GameObject m_hitSceneParent;
+
+	// Use this for initialization
+	void Start () {
+		StopARSessionRemoveAnchor ();
+		if (GlobalForChangeScene.IsPositionAssigned) {
+			m_hitSceneParent.SetActive (true);
+			m_hitSceneParent.transform.position = GlobalForChangeScene.globalPosition;
+			m_hitSceneParent.transform.rotation = GlobalForChangeScene.globalRotation;
+		}
+	}
+
+	void StopARSessionRemoveAnchor()
+	{
+		#if !UNITY_EDITOR
+		m_config.alignment = UnityARAlignment.UnityARAlignmentGravityAndHeading;
+		m_config.planeDetection = UnityARPlaneDetection.None;
+		m_config.enableLightEstimation = true;
+		m_config.getPointCloudData = false;
+		m_RunOption = UnityARSessionRunOption.ARSessionRunOptionRemoveExistingAnchors;
+		m_Session.RunWithConfigAndOptions (m_config, m_RunOption);
+		#endif
+	}
+}
---
+++ Assets/Scripts/DontDestroyOnLoad.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyOnLoad : MonoBehaviour {
+
+	private static DontDestroyOnLoad _instance;
+
+	public static DontDestroyOnLoad Instance
+	{
+		get
+		{
+			if (!_instance) {
+				_instance = GameObject.FindObjectOfType (typeof(DontDestroyOnLoad)) as DontDestroyOnLoad;
+				if(!_instance)
+				{
+					GameObject am = new GameObject ("DontDestroyOnLoad");
+					_instance = am.AddComponent (typeof(DontDestroyOnLoad)) as DontDestroyOnLoad;
+				}
+			}
+			return _instance;
+		}
+	}
+
+	void Awake()  
+	{  
+		//此脚本永不消毁，并且每次进入初始场景时进行判断，若存在重复的则销毁  
+		if (_instance == null)  
+		{  
+			_instance = this;  
+			DontDestroyOnLoad(this);  
+		} 
+		else if (this != _instance)  
+		{  
+			Destroy(gameObject);  
+		}
+	}  
+}
