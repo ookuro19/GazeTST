88917ce3daa932fac7dc2765e2344d39e6d8f3b3
NeunZehn <nzdream@163.com>
2017-11-19 22:52:19 +0800

    Initiate after a few seconds

---
+++ Assets/Scripts/GameCtrl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCtrl : MonoBehaviour {
+
+	public GameObject CharacterParent;
+
+	// Use this for initialization
+	void Start () {
+		Invoke("InstantiateCharacterParent", 1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void InstantiateCharacterParent()
+	{
+		Instantiate (CharacterParent, Vector3.forward * 3f, Quaternion.identity);
+	}
+}
